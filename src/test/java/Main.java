29d0efd660846eb88a4a4fd961e8742052dfcc06
Olha <olyarozhko@gmail.com>
2024-08-04 23:40:03 +0300

    all test classes are added to run from the Main.class

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,13 +1,14 @@
 import org.testng.TestListenerAdapter;
 import org.testng.TestNG;
+import restApi.TestESims;
+import restApi.TestOrders;
 import ui.TestESimPackageDetails;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!!!");
         TestListenerAdapter tla = new TestListenerAdapter();
         TestNG testng = new TestNG();
-        testng.setTestClasses(new Class[] {TestESimPackageDetails.class});
+        testng.setTestClasses(new Class[] {TestESimPackageDetails.class, TestOrders.class, TestESims.class});
         testng.addListener(tla);
         testng.run();
     }
